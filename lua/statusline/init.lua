7138616148c055e11e41c844db21bd019b886adf
pita <pita092@github.com>
2024-12-13 23:28:53 +0200

    text-text text text text...

--- lua/statusline/init.lua
+++ lua/statusline/init.lua
@@ -1,3 +1,150 @@
+-- local config = {
+--   colors = "gruvbox",
+--   padding = 6,
+--   separator = {
+--     enabled = true,
+--     separator = "",
+--   },
+--   git = {
+--     enabled = true,
+--     icon = "",
+--   },
+--   scroll = {
+--     enabled = true,
+--   },
+--   words = {
+--     enabled = true,
+--   },
+--   mode = {
+--     enabled = true,
+--   },
+--   filename = {
+--     enabled = true,
+--     path_type = "relative",
+--   },
+-- }
+--
+-- local git = require("components.git")
+-- local scroll = require("components.scrollbar")
+-- local words = require("components.words")
+-- local mode = require("components.mode")
+-- local filename = require("components.filename")
+-- local separator = require("components.separators")
+--
+-- local function a(x)
+--   return string.rep(" ", x)
+-- end
+--
+-- local M = {}
+--
+-- local function load_colors()
+--   local ok, _ = pcall(require, "colors." .. config.colors)
+--   if not ok then
+--     vim.notify("Failed to load color scheme: " .. config.colors, vim.log.levels.WARN)
+--   end
+-- end
+--
+-- function M.setup(user_config)
+--   -- Check if user_config.config exists and use it, otherwise use user_config directly
+--   local new_config = user_config.config or user_config
+--
+--   -- Merge user config with default config
+--   config = vim.tbl_deep_extend("force", config, new_config or {})
+--
+--   -- Pass configurations to individual modules
+--   git.setup(config.git)
+--   scroll.setup(config.scroll)
+--   words.setup(config.words)
+--   mode.setup(config.mode)
+--   filename.setup(config.filename)
+--   separator.setup(config.separator)
+--
+--   vim.o.statusline = '%!v:lua.require("statusline").set_statusline()'
+--
+--   load_colors()
+-- end
+--
+-- function M.set_statusline()
+--   local components = {
+--     '%#StatusLine_bg#',
+--     string.rep(" ", config.padding),
+--   }
+--
+--   if config.mode.enabled then
+--     table.insert(components, mode.mode_indicator(config.mode))
+--   end
+--
+--   table.insert(components, '%#StatusLine_Normal# ')
+--
+--   if config.separator.enabled then
+--     table.insert(components, '%#StatusLine_Separatror#')
+--     table.insert(components, separator.seps(config.separator))
+--     table.insert(components, '%#StatusLine_Normal# ')
+--   end
+--
+--   table.insert(components, '%#StatusLine_Column#col:%c%#StatusLine_Normal#')
+--   table.insert(components, a(2))
+--
+--   if config.separator.enabled then
+--     table.insert(components, '%#StatusLine_Separatror#')
+--     table.insert(components, separator.seps(config.separator))
+--     table.insert(components, '%#StatusLine_Normal# ')
+--   end
+--
+--   if config.filename.enabled then
+--     table.insert(components, filename.custom_filename(config.filename))
+--   end
+--
+--   if config.separator.enabled then
+--     table.insert(components, '%#StatusLine_Separatror#')
+--     table.insert(components, separator.seps(config.separator))
+--     table.insert(components, '%#StatusLine_Normal# ')
+--   end
+--
+--   table.insert(components, a(1))
+--   table.insert(components, '%=')
+--   table.insert(components, '%#StatusLine_Normal#')
+--
+--   if config.scroll.enabled then
+--     table.insert(components, '%#Scrollbar#')
+--     table.insert(components, scroll.scrollbar_indicator(config.scroll))
+--     table.insert(components, '%#StatusLine_Normal# ')
+--   end
+--
+--   if config.separator.enabled then
+--     table.insert(components, '%#StatusLine_Separatror#')
+--     table.insert(components, separator.seps(config.separator))
+--     table.insert(components, '%#StatusLine_Normal# ')
+--   end
+--
+--   table.insert(components, a(1))
+--
+--   if config.words.enabled then
+--     table.insert(components, words.word_count(config.words))
+--     table.insert(components, a(1))
+--   end
+--
+--   if config.git.enabled and git.in_git_repo() then
+--     table.insert(components, '%#StatusLine_bg#')
+--     table.insert(components, a(2))
+--     table.insert(components, '%#StatusLine_GitStatus#')
+--     table.insert(components, git.get_branch(config.git))
+--     table.insert(components, a(1))
+--   end
+--
+--   table.insert(components, '%#StatusLine_bg#')
+--   table.insert(components, a(config.padding - 1))
+--
+--   return table.concat(components, '')
+-- end
+--
+-- git.init_git_branch()
+--
+-- return M
+
+
+local lualine = require('lualine')
+
 local config = {
   colors = "gruvbox",
   padding = 6,
@@ -31,10 +178,6 @@ local mode = require("components.mode")
 local filename = require("components.filename")
 local separator = require("components.separators")
 
-local function a(x)
-  return string.rep(" ", x)
-end
-
 local M = {}
 
 local function load_colors()
@@ -42,6 +185,58 @@ local function load_colors()
   if not ok then
     vim.notify("Failed to load color scheme: " .. config.colors, vim.log.levels.WARN)
   end
+end
+
+local function setup_lualine()
+  lualine.setup({
+    options = {
+      theme = config.colors,
+      component_separators = config.separator.enabled and config.separator.separator or "",
+      section_separators = config.separator.enabled and config.separator.separator or "",
+      globalstatus = true,
+    },
+    sections = {
+      lualine_a = {
+        {
+          function() return mode.mode_indicator(config.mode) end,
+          padding = { left = config.padding, right = 1 }
+        },
+      },
+      lualine_b = {
+        {
+          function() return git.get_branch(config.git) end,
+          icon = config.git.icon,
+          cond = git.in_git_repo
+        },
+      },
+      lualine_c = {
+        {
+          function() return filename.custom_filename(config.filename) end
+        },
+      },
+      lualine_x = {
+        {
+          function() return words.word_count(config.words) end
+        },
+      },
+      lualine_y = {
+        {
+          function() return scroll.scrollbar_indicator(config.scroll) end
+        },
+      },
+      lualine_z = {
+        { 'location', padding = { right = config.padding - 1 } },
+      },
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { { function() return filename.custom_filename(config.filename) end } },
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = {},
+    },
+  })
 end
 
 function M.setup(user_config)
@@ -59,83 +254,8 @@ function M.setup(user_config)
   filename.setup(config.filename)
   separator.setup(config.separator)
 
-  vim.o.statusline = '%!v:lua.require("statusline").set_statusline()'
-
   load_colors()
-end
-
-function M.set_statusline()
-  local components = {
-    '%#StatusLine_bg#',
-    string.rep(" ", config.padding),
-  }
-
-  if config.mode.enabled then
-    table.insert(components, mode.mode_indicator(config.mode))
-  end
-
-  table.insert(components, '%#StatusLine_Normal# ')
-
-  if config.separator.enabled then
-    table.insert(components, '%#StatusLine_Separatror#')
-    table.insert(components, separator.seps(config.separator))
-    table.insert(components, '%#StatusLine_Normal# ')
-  end
-
-  table.insert(components, '%#StatusLine_Column#col:%c%#StatusLine_Normal#')
-  table.insert(components, a(2))
-
-  if config.separator.enabled then
-    table.insert(components, '%#StatusLine_Separatror#')
-    table.insert(components, separator.seps(config.separator))
-    table.insert(components, '%#StatusLine_Normal# ')
-  end
-
-  if config.filename.enabled then
-    table.insert(components, filename.custom_filename(config.filename))
-  end
-
-  if config.separator.enabled then
-    table.insert(components, '%#StatusLine_Separatror#')
-    table.insert(components, separator.seps(config.separator))
-    table.insert(components, '%#StatusLine_Normal# ')
-  end
-
-  table.insert(components, a(1))
-  table.insert(components, '%=')
-  table.insert(components, '%#StatusLine_Normal#')
-
-  if config.scroll.enabled then
-    table.insert(components, '%#Scrollbar#')
-    table.insert(components, scroll.scrollbar_indicator(config.scroll))
-    table.insert(components, '%#StatusLine_Normal# ')
-  end
-
-  if config.separator.enabled then
-    table.insert(components, '%#StatusLine_Separatror#')
-    table.insert(components, separator.seps(config.separator))
-    table.insert(components, '%#StatusLine_Normal# ')
-  end
-
-  table.insert(components, a(1))
-
-  if config.words.enabled then
-    table.insert(components, words.word_count(config.words))
-    table.insert(components, a(1))
-  end
-
-  if config.git.enabled and git.in_git_repo() then
-    table.insert(components, '%#StatusLine_bg#')
-    table.insert(components, a(2))
-    table.insert(components, '%#StatusLine_GitStatus#')
-    table.insert(components, git.get_branch(config.git))
-    table.insert(components, a(1))
-  end
-
-  table.insert(components, '%#StatusLine_bg#')
-  table.insert(components, a(config.padding - 1))
-
-  return table.concat(components, '')
+  setup_lualine()
 end
 
 git.init_git_branch()
