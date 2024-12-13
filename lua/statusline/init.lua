fa425b040927745d9189cd74ade1fbddd9592d8a
pita <pita092@github.com>
2024-12-13 16:46:56 +0200

    text-text text text text...

--- lua/statusline/init.lua
+++ lua/statusline/init.lua
@@ -123,7 +123,6 @@ function M.set_statusline()
     table.insert(components, a(2))
     table.insert(components, '%#StatusLine_GitStatus#')
     table.insert(components, config.config.git.icon)
-    table.insert(components, a(1))
     table.insert(components, git.get_branch(config.git))
     table.insert(components, a(1))
   end
