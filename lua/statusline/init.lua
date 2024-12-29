376d3b38cefb3d722c9029e7d618308e01d6e919
Your Name <you@example.com>
2024-12-29 21:39:54 +0000

    text text-text text...

--- lua/statusline/init.lua
+++ lua/statusline/init.lua
@@ -144,7 +144,7 @@ function M.set_statusline()
   end
 
   table.insert(components, '%#StatusLine_bg#')
-  -- table.insert(components, a(config.padding - 1))
+  table.insert(components, a(config.padding - 1))
   -- table.insert(components, lsp.lsp_status())
 
   return table.concat(components, '')
